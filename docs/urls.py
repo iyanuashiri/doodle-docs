df02eb776c0e8ffd5c9f5b3a8288077e50de0a39
Ashiri <ajaoiyanu@gmail.com>
2019-03-26 14:51:23 +0100

    Add urls and routers

---
+++ docs/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from . import viewsets, views
+
+
+router = DefaultRouter()
+router.register('', viewsets.DocViewset)
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+    
+    path('', views.DocList.as_view()),
+    path('/<int:pk>/', views.DocDetail.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
