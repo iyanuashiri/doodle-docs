7e1e13a8264f454743f6ba77e1c5a23b9e0f3fbc
Ashiri <ajaoiyanu@gmail.com>
2019-03-26 14:45:53 +0100

    Add api views

---
+++ docs/views.py
@@ -0,0 +1,24 @@
+from rest_framework import permissions, generics
+
+from .serializers import DocSerializer
+from .models import Doc
+
+# Create your views here.
+
+
+class DocList(generics.ListCreateAPIView):
+    """
+    List all docs and create a new doc
+    """
+    serializer_class = DocSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    queryset = Doc.objects.all()
+
+
+class DocDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, Edit and Delete a Doc instance
+    """
+    serializer_class = DocSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    queryset = Doc.objects.all()
