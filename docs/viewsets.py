307f25678cf9b336daff2c0495fbe355cb2b0c7d
Ashiri <ajaoiyanu@gmail.com>
2019-03-26 14:34:52 +0100

    Create DocViewset

---
+++ docs/viewsets.py
@@ -0,0 +1,14 @@
+from rest_framework import viewsets, permissions
+
+from .serializers import DocSerializer
+from .models import Doc
+
+
+class DocViewset(viewsets.ModelViewSet):
+    """
+    This endpoint provides list, detail, retrieve, create and delete actions for Doc
+    """
+
+    queryset = Doc.objects.all()
+    serializer_class = DocSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
