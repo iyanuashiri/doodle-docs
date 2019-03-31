d075ff0e5a83f4474e324dffef8ad3465918d13f
Ashiri <ajaoiyanu@gmail.com>
2019-03-31 18:59:19 +0100

    Configure global pagination

--- doodle_docs/settings.py
+++ doodle_docs/settings.py
@@ -128,3 +128,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
