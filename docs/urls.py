3c5766e031262878b9332b751621aaf8c1156a46
Ashiri <ajaoiyanu@gmail.com>
2019-03-31 19:24:59 +0100

    Add name urls

--- docs/urls.py
+++ docs/urls.py
@@ -2,8 +2,12 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.schemas import get_schema_view
 
 from . import viewsets, views
+
+
+schema_view = get_schema_view(title='Docs API')
 
 
 router = DefaultRouter()
@@ -12,9 +16,10 @@ router.register('', viewsets.DocViewset)
 app_name = 'docs'
 urlpatterns = [
     path('v1/', include(router.urls)),
+    path('schema/', schema_view),
 
     path('v2/', views.DocList.as_view()),
-    path('v2/<int:pk>/', views.DocDetail.as_view()),
+    path('v2/<int:pk>/', views.DocDetail.as_view(), name='detail'),
 ]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+# urlpatterns = format_suffix_patterns(urlpatterns)
