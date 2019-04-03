1db0cb8db673979157c3dfbe3dd7920a151aa6ab
Ashiri <ajaoiyanu@gmail.com>
2019-04-03 12:52:44 +0100

    Add djoser

--- doodle_docs/settings.py
+++ doodle_docs/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders',
+    'djoser',
 
     'docs',
 ]
@@ -131,6 +133,9 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication'
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
