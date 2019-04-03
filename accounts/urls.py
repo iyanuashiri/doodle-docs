0843fcdd0b805e5b32e0c8395488452942e856ed
Ashiri <ajaoiyanu@gmail.com>
2019-04-03 15:38:05 +0100

    Add accounts api

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,22 @@
 from django.urls import path, include
 
+from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
+
+from . import viewsets
+
+
+schema_view = get_schema_view(title='Accounts API')
+
+router = DefaultRouter()
+router.register('', viewsets.AccountViewset)
+
 
 app_name = 'accounts'
 urlpatterns = [
+    path('accounts/', include(router.urls)),
+    path('schema/', schema_view),
+
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 
---
+++ accounts/viewsets.py
@@ -0,0 +1,13 @@
+from rest_framework import viewsets, permissions
+
+from .serializers import AccountSerializer
+from .models import Account
+
+
+class AccountViewset(viewsets.ReadOnlyModelViewSet):
+    """
+    This endpoint provides list, detail actions for Account
+    """
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
