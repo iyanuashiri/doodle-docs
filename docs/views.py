5cd5e999d028aae28b3aa1aea9b7522b5ab7ab16
Ashiri <ajaoiyanu@gmail.com>
2019-04-03 14:43:09 +0100

    Add share doc function

--- docs/views.py
+++ docs/views.py
@@ -1,5 +1,10 @@
-from rest_framework import permissions, generics
+from django.shortcuts import get_object_or_404
 
+from rest_framework import permissions, generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from accounts.models import Account
 from .serializers import DocSerializer
 from .models import Doc
 
@@ -11,7 +16,7 @@ class DocList(generics.ListCreateAPIView):
     List all docs and create a new doc
     """
     serializer_class = DocSerializer
-    # permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     queryset = Doc.objects.all()
 
 
@@ -20,5 +25,16 @@ class DocDetail(generics.RetrieveUpdateDestroyAPIView):
     Retrieve, Edit and Delete a Doc instance
     """
     serializer_class = DocSerializer
-    # permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     queryset = Doc.objects.all()
+
+
+class DocShare(APIView):
+    """
+    Share a doc instance with another user
+    """
+    def post(self, request, user_id, doc_id):
+        doc = get_object_or_404(Doc, pk=doc_id)
+        user = get_object_or_404(Account, user_id)
+        user.doc_shared.add(doc)
+        return Response({'shared': True})
