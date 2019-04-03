4a788c185b906a03250858b548c5e5592dc43b36
Ashiri <ajaoiyanu@gmail.com>
2019-04-03 15:24:45 +0100

    Add nw fields to Doc model

--- docs/models.py
+++ docs/models.py
@@ -1,14 +1,17 @@
 from django.db import models
+from django.conf import settings
 from django.shortcuts import reverse
 
 # Create your models here.
 
 
 class Doc(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='notes')
     title = models.CharField(max_length=255)
     body = models.TextField(max_length=40000)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    authors_shared = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='docs_shared', blank=True)
 
     class Meta:
         verbose_name = 'doc'
--- docs/serializers.py
+++ docs/serializers.py
@@ -3,10 +3,9 @@ from rest_framework import serializers
 from .models import Doc
 
 
-class DocSerializer(serializers.ModelSerializer):
-    url = serializers.CharField(source='get_absolute_url', read_only=True)
+class DocSerializer(serializers.HyperlinkedModelSerializer):
+    author = serializers.ReadOnlyField(source='author.email')
 
     class Meta:
         model = Doc
-        fields = ('id', 'url', 'title', 'body', 'created_at', 'updated_at')
-
+        fields = ('id', 'url', 'author', 'title', 'body', 'created_at', 'updated_at', 'authors_shared')
