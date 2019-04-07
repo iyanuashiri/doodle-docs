9d68830a107b9bb8139fb19aeb2313b70e341d61
Ashiri <ajaoiyanu@gmail.com>
2019-04-08 00:01:52 +0100

    Fix rest authentication with comma trailing

--- doodle_docs/settings.py
+++ doodle_docs/settings.py
@@ -137,7 +137,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication'
+        'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
